8112195a244e74940709c881d1e57dd863ffc0d9
Kaleb Boothe <kboothe617@gmail.com>
2024-08-05 17:44:23 -0600

    Truck.ts override error fix

--- Develop/src/classes/Truck.ts
+++ Develop/src/classes/Truck.ts
@@ -10,14 +10,14 @@ class Truck extends Vehicle implements AbleToTow {
   // TODO: Declare properties of the Truck class
   // TODO: The properties should include vin, color, make, model, year, weight, top speed, wheels, and towing capacity
   // TODO: The types should be as follows: vin (string), color (string), make (string), model (string), year (number), weight (number), topSpeed (number), wheels (Wheel[]), towingCapacity (number)
-  vin: string;
-  color: string;
-  make: string;
-  model: string;
-  year: number;
-  weight: number;
-  topSpeed: number;
-  wheels: Wheel[];
+  override vin: string;
+  override color: string;
+  override make: string;
+  override model: string;
+  override year: number;
+  override weight: number;
+  override topSpeed: number;
+  override wheels: Wheel[];
   towingCapacity: number;
   // TODO: Create a constructor that accepts the properties of the Truck class
     // TODO: The constructor should call the constructor of the parent class, Vehicle
